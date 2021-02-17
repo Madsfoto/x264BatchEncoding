eac86b529a0f9886eda156b9db231a4b982590f4
Mads Johansen <mads@madsfoto.dk>
2021-02-17 13:25:48 +0100

    Move out of function

--- Program.cs
+++ Program.cs
@@ -9,8 +9,20 @@ namespace x264BatchEncoding
 {
     class Program
     {
-        static void Loops(string inputFilename)
+     
+        static void Main(string[] args)
         {
+            string inputFilename = "";
+            if(args.Length==0)
+            {
+                inputFilename = "ColorCube.mov";
+                Console.WriteLine("Defaulting to ColorCube.mov, use 1st argument for other name");
+            }
+            else
+            {
+                inputFilename = args[1];
+            }
+
             string[] profileArr = { "baseline", "main", "high", "high10", "high422", "high444" };
             string[] presetArr = { "ultrafast", "superfast", "veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow", "placebo" };
             string[] pix_fmtArr = { "yuv420p", "yuv422p", "yuv444p", "yuv420p10le", "yuv422p10le", "yuv444p10le" };
@@ -38,33 +50,33 @@ namespace x264BatchEncoding
                             {// add profile + format, preset and crf to FFmpeg comand, add encode number to output filename
 
                                 // In baseline only yuv420p is supported
-                                if (profileInt==0 && pix_fmtInt!=0)
+                                if (profileInt == 0 && pix_fmtInt != 0)
                                 {
                                     break;
                                 }
                                 // in main and high, no 10 bit format is supported
-                                if((profileInt==1||profileInt==2) && (pix_fmtInt==3||pix_fmtInt==4||pix_fmtInt==5))
+                                if ((profileInt == 1 || profileInt == 2) && (pix_fmtInt == 3 || pix_fmtInt == 4 || pix_fmtInt == 5))
                                 {
                                     break;
                                 }
                                 // in high10 only up to 420p10le is supported
-                                if(profileInt==3 && (pix_fmtInt==1||pix_fmtInt==2||pix_fmtInt==4||pix_fmtInt==5))
+                                if (profileInt == 3 && (pix_fmtInt == 1 || pix_fmtInt == 2 || pix_fmtInt == 4 || pix_fmtInt == 5))
                                 {
                                     break;
                                 }
                                 // high422 (should be named high10422, as it's high10 + 422 subsampling support
-                                if(profileInt==4 && (pix_fmtInt==2||pix_fmtInt==5))
+                                if (profileInt == 4 && (pix_fmtInt == 2 || pix_fmtInt == 5))
                                 {
                                     break;
                                 }
-                                // high444 supports everything
+                                // high444, again high10444, supports everything
 
 
                                 string FFCmd = ff + profileStr + profileArr[profileInt] + " " + presetStr + presetArr[presetInt] + " " + CRFStr + CRFInt + " " + pix_fmtStr + pix_fmtArr[pix_fmtInt];
 
-                                outputFilename = inputFilenameNoExt + "_" +profileInt+ profileArr[profileInt] + "_" + presetInt+presetArr[presetInt] + "_" + CRFInt + "_" + pix_fmtArr[pix_fmtInt] + "_" + encodeInt + ".mov";
+                                outputFilename = inputFilenameNoExt + "_" + profileInt + profileArr[profileInt] + "_" + presetInt + presetArr[presetInt] + "_" + CRFInt + "_" + pix_fmtArr[pix_fmtInt] + "_" + encodeInt + ".mov";
 
-                                writeStrList.Add(FFCmd + " "+outputFilename);
+                                writeStrList.Add(FFCmd + " " + outputFilename);
                             }
 
                         }
@@ -72,32 +84,14 @@ namespace x264BatchEncoding
 
                     }
 
-                    File.WriteAllLines("0-" + profileInt + profileArr[profileInt] + presetInt +"-"+ presetInt+ presetArr[presetInt] + ".bat", writeStrList);
+                    File.WriteAllLines("0-" + profileInt + profileArr[profileInt] + presetInt + "-" + presetInt + presetArr[presetInt] + ".bat", writeStrList);
                     writeStrList.Clear();
                 }
 
             } // last loop done
 
+
             
-
-        }
-        static void Main(string[] args)
-        {
-            string inputFilename = "";
-            if(args.Length==0)
-            {
-                inputFilename = "ColorCube.mov";
-                Console.WriteLine("Defaulting to ColorCube.mov, use 1st argument for other name");
-            }
-            else
-            {
-                inputFilename = args[1];
-            }
-
-
-
-            Loops(inputFilename);
-
             
             
 
